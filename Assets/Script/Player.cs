321669b9041c4f3c72f0224868469a4d96a5eaaf
Vpatylergl@gmail.com <Vpatylergl@gmail.com>
2023-10-30 19:27:44 -0400

    Added Laser Script

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -6,7 +6,13 @@ public class Player : MonoBehaviour
 {
 
     [SerializeField]
-    private float _speed = 3.5f; 
+    private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    [SerializeField]
+    private float _canFire = -1f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +24,11 @@ public class Player : MonoBehaviour
     void Update()
     {
         CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
     }
     void CalculateMovement()
     {
@@ -39,5 +50,11 @@ public class Player : MonoBehaviour
 
             transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
+    }
+
+    void FireLaser()
+    {
+            _canFire = Time.time + _fireRate;
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
     }
 }
