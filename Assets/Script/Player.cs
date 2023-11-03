183b87fa807610517f638734d3d32b3ad2e3ffda
Vpatylergl@gmail.com <Vpatylergl@gmail.com>
2023-11-03 18:21:16 -0400

    Assets Completed

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -10,9 +10,9 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameObject _laserPrefab;
     [SerializeField]
-    private float _fireRate = 0.5f;
+    private float _fireRate = 1.5f;
     [SerializeField]
-    private float _canFire = -1f;
+    private float _canFire = -.5f;
     [SerializeField]
     private int _lives = 3;
     [SerializeField]
@@ -22,7 +22,7 @@ public class Player : MonoBehaviour
     void Start()
     {
         transform.position = new Vector3(0, 0, 0);
-        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+        _spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
             //find the object. get the component 
             
 
@@ -63,7 +63,7 @@ public class Player : MonoBehaviour
     void FireLaser()
     {
             _canFire = Time.time + _fireRate;
-            Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
     }
 
     public void Damage()
@@ -80,6 +80,7 @@ public class Player : MonoBehaviour
             //Communication with Spawn Manager
             //SpawnManager spawnManger = //Find the GameObject. Then get component
             //Let them know to stop spawning
+            _spawnManager.OnPlayerDeath();
             Destroy(this.gameObject);
         }
     }
